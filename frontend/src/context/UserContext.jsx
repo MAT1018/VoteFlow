47ce05eb9ba2c7cd7945a32dbcbbcfb0ad298c08
Athar Ramzan <atharramzan.ofi@gmail.com>
2025-05-27 02:58:35 +0530

    poll ui and bookmark

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 export const UserContext = createContext();
 
 const UserProvider = ({ children })=>{
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
 
     //Function to update user data
     const updateUser = (userData)=>{
@@ -21,7 +21,13 @@ const UserProvider = ({ children })=>{
             ...prev,
             [key]: value,
         }))
-    }
+    };
+
+    //Update totalVotes count locally
+    const onUserVoted = () => {
+        const  totalPollsVotes = user.totalPollsVotes || 0;
+        updateUserStats("totalPollsVotes", totalPollsVotes + 1);
+    };
 
     //Update totalPollsCreated count locally
     const onPollCreateOrDelete = ( type = "create" ) => {
@@ -31,13 +37,41 @@ const UserProvider = ({ children })=>{
             type == "create" ? totalPollsCreated + 1 : totalPollsCreated - 1
         )
     }
+//Add or Remove poll id from bookmarkedPolls
+   const toggleBookmarkId = (pollId) =>{
 
+       const bookmarks = user.bookmarkedPolls || [];
+
+       const index = bookmarks.indexOf(pollId);
+
+       if(index === -1){
+        //Add the ID if it's not in the array
+        setUser((prev) => ({
+            ...prev,
+            bookmarkedPolls:[...bookmarks, pollId],
+            totalPollsBookmarked:
+            prev.totalPollsBookmarked + 1,
+        }))
+
+       }else{
+          //Remove the ID if its already in the array
+          setUser((prev)=>({
+            ...prev,
+            bookmarkedPolls:
+            bookmarks.filter((item)=> item !== pollId),
+            totalPollsBookmarked:
+            prev.totalPollsBookmarked -1,
+          }));
+       }
+   };
     return <UserContext.Provider
     value={{
         user,
         updateUser,
         clearUser,
         onPollCreateOrDelete,
+        onUserVoted,
+        toggleBookmarkId,
     }}
     >{ children }</UserContext.Provider>
 }
