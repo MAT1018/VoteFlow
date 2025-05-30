5bb54386715a7a49617b34657d960e83d798e594
Ayaan <ayaan.taimur1@gmail.com>
2025-05-31 01:30:58 +0530

    render is added

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'http://localhost:8000'
+// export const BASE_URL = 'http://localhost:8000'
+
+export const BASE_URL = 'https://voteflow.onrender.com/' 
 
 export const API_PATHS = {
     AUTH:{
